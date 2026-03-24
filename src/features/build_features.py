3cd8ac572fb180f478e8a2d7cc593a99f8ca2f5b
Ryan Huang <ryanhuang6391@gmail.com>
2026-03-23 23:55:12 -0700

    feature engineering code

---
+++ src/features/build_features.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def build_features(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Apply complete feature engineering pipeline on training data
+    """
+    df = df.copy()
+    df["text"] = df["description"].astype(str) + "\n" + df["topics"].astype(str) + "\n" + df["keywords"].astype(str) + "\n" + df["videos"].astype(str) 
+    df = df.drop(["description", "topics", "keywords", "videos"], axis=1)
+
+    return df;
