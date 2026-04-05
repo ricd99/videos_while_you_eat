1982fe886a19b05cbdec1a437f119c9ccc6bf7c0
Ryan Huang <ryanhuang6391@gmail.com>
2026-04-04 23:33:47 -0700

    channel features function

--- src/data/youtube_api.py
+++ src/data/youtube_api.py
@@ -16,9 +16,35 @@ def get_channel_id_from_name(channel_name: str) -> str | None:
     ).execute()
 
     items = resp.get("items", [])
-    if not items:
+    if not resp.get("items"):
         print(f"could not find channel: {channel_name}")
         return None
     
     return items[0]["snippet"]["channelId"] #TODO: return options for user to choose
 
+def get_channel_features(channel_id: str) -> dict | None:
+    resp = yt.channels().list(
+        part="brandingSettings,contentDetails,snippet,topicDetails",
+        id=channel_id
+    ).execute()
+
+    items = resp.get("items", [])
+    if not resp.get("items"):
+        print(f"could not find channel: {channel_id}")
+        return None
+    
+    ch = resp["items"][0]
+    snippet = ch.get("snippet", {})
+    topic = ch.get("topicDetails", {})
+    branding = ch.get("brandingSettings", {}).get("channel", {})
+    uploads_playlist = ch.get("contentDetails", {}).get("relatedPlaylists", {})
+
+    return {
+        "channel_name": snippet.get("title"),
+        "title":        snippet.get("title"),
+        "description":  snippet.get("description"),
+        "country":      snippet.get("country"),
+        "topics":       topic.get("topicCategories"),
+        "keywords":     branding.get("keywords"),
+        "uploads":      uploads_playlist.get("uploads"),
+    }
