36d337d1ee900c49af7a8f21f45b1b0f7749723c
Ryan Huang <ryanhuang6391@gmail.com>
2026-04-04 23:42:04 -0700

    refactored inference to call yt api

--- src/serving/inference.py
+++ src/serving/inference.py
@@ -2,18 +2,23 @@ import joblib
 from pathlib import Path
 import pandas as pd
 
+from src.data.youtube_api import get_channel_data
 from src.data.preprocess_data import preprocess_data
 from src.features.build_features import build_features
+
 
 ct = joblib.load(Path.cwd() / "src" / "serving" / "model" / "eacd9855d8444a0fad5bd82d2629fb78" / "artifacts" / "column_transformer.pkl")
 nn = joblib.load(Path.cwd() / "src" / "serving" / "model" / "eacd9855d8444a0fad5bd82d2629fb78" / "artifacts" / "nn_model.pkl")
 DATA_PATH = Path.cwd() / "data" / "processed" / "channels_pp.csv"
 df_all = pd.read_csv(DATA_PATH)
 
-def predict(input_dict: dict):
+def predict(channel_name: str):
+    input_dict = get_channel_data(channel_name)
+    if input_dict is None:
+        return {"error": f"could not find channel: {channel_name}"}
+    
     df = pd.DataFrame([input_dict])
     
-    # same steps as training pipeline
     df = preprocess_data(df)
     df = build_features(df)
     df_transformed = ct.transform(df)
