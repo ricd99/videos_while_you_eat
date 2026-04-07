889d7c903d50ba10b59dd30d867969b3b0d8dfc4
Ryan Huang <ryanhuang6391@gmail.com>
2026-04-06 22:42:35 -0700

    add consts to git ignore

--- scripts/rds_data_collection/collect_channels.py
+++ scripts/rds_data_collection/collect_channels.py
@@ -9,3 +9,8 @@ load_dotenv()
 
 yt = build("youtube", "v3", developerKey=os.getenv("YOUTUBE_API_KEY"))
 s3 = boto3.client("s3", region_name="us-west-2")
+
+BUCKET = "ytrec-data-lake"
+MIN_SUBSCRIBERS = 1000
+MIN_VIDEOS = 10
+MONTHS_INACTIVE = 6
