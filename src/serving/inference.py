7cb35599e2ace474ff6f9e8413e876e761296803
Ryan Huang <ryanhuang6391@gmail.com>
2026-04-06 21:16:34 -0700

    added sqlalchemy

--- src/serving/inference.py
+++ src/serving/inference.py
@@ -16,22 +16,35 @@ nn = joblib.load(Path.cwd() / "src" / "serving" / "model" / "eacd9855d8444a0fad5
 
 def _get_db_connection():
     return psycopg2.connect(
-        host=os.getenv("RDS_HOST")
+        host=os.getenv("RDS_HOST"),
+        port=5432,
+        database="postgres",
+        user="postgres",
+        password=os.getenv("RDS_PW"),
+        sslmode="require"
     )
 
+def _load_lookup_table() -> pd.DataFrame:
+    conn = _get_db_connection()
+    df = pd.read_sql("SELECT channel_id, channel_name FROM channels_cleaned;", conn)
+    conn.close()
+    return df
+
+
 def predict(channel_name: str) -> list[dict] | dict:
+    df_lookup = _load_lookup_table()
+
     input_dict = get_channel_data(channel_name)
     if input_dict is None:
         return {"error": f"could not find channel: {channel_name}"}
     
     df = pd.DataFrame([input_dict])
-    
     df = preprocess_data(df)
     df = build_features(df)
     df_transformed = ct.transform(df)
     
     distances, indices = nn.kneighbors(df_transformed)
     
-    results = df_all.iloc[indices[0]][["channel_name", "channel_id"]].copy()
+    results = df_lookup.iloc[indices[0]][["channel_name", "channel_id"]].copy()
     results["similarity_score"] = distances[0]
     return results.to_dict(orient="records")
