a5b97cdf8dba08f5dfb79549077ff6c7e72dfb64
ricd99 <ryanhuang6391@gmail.com>
2026-04-27 20:54:54 -0700

    tune.py stub

--- src/models/tune.py
+++ src/models/tune.py
@@ -1,48 +1,17 @@
-import optuna 
+import mlflow 
 from sklearn.neighbors import NearestNeighbors
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.compose import make_column_transformer
+import numpy as np
 
-def tune_model(df_train, df_test):
+def tune_model(df_train: np.ndarray, df_test):
     """
-    Tunes CountVectorizer + NearestNeighbors using Optuna
+    Tune NearestNeighbors parameters (stub implementation).
+    Since sentence embeddings are pre-trained, there are no CountVectorizer params
+    to optimize. This function returns fixed values and logs with MLFlow
+    Args:
+        df_train: Embedding matrix from training data
+        df_test: Embedding matrix from test data
     """
 
-    def objective(trial):
-
-        max_features = trial.suggest_int("max_features", 100, 2000, step=100)
-        min_df       = trial.suggest_int("min_df", 1, 5)
-        max_df       = trial.suggest_float("max_df", 0.7, 1.0, step=0.05)
-        ngram_max    = trial.suggest_int("ngram_max", 1, 3) 
-        metric       = trial.suggest_categorical("metric", ["cosine", "euclidean"])
-
-        ct = make_column_transformer(
-            (CountVectorizer(
-                stop_words="english",
-                max_features=max_features,
-                min_df=min_df,
-                max_df=max_df,
-                ngram_range=(1, ngram_max)
-            ), "text"),
-            ("drop", ["channel_id", "channel_name"]),
-        )
-
-        try:
-            df_train_pp = ct.fit_transform(df_train)
-            df_test_pp  = ct.transform(df_test)
-        except ValueError:
-            # Some param combos produce an empty vocabulary
-            return float("inf")
-
-        nn = NearestNeighbors(n_neighbors=5, metric=metric)
-        nn.fit(df_train_pp)
-
-        distances, _ = nn.kneighbors(df_test_pp)
-        mean_dist = distances.mean()
-
-        return mean_dist 
-
-    study = optuna.create_study(direction="minimize")
-    study.optimize(objective, n_trials=20, show_progress_bar=True)
-
-    return study.best_params
+    params = {"n_neighbors": 15, "metric": "cosine"}
+    mlflow.log_params(params)
+    return params
